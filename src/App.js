5e8dd1dc1b922be8ca0ace639a681042cef01cdd
Rubens Albergaria <rubensalbergaria@gmail.com>
2021-03-19 17:25:58 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div id="header">
+        <h1>Cryptoland</h1>
+      </div>  
+
+      <div className="cryptolist">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>    
+    </div>
+  );
+}
+
+export default App;
